6469efd9d1ef6691db5e4bef3e0e142b88b4a039
solene <solene.granchi@etu.u-bordeaux.fr>
2026-06-03 16:56:40 +0200

    refactoring

--- public/src/log.js
+++ public/src/log.js
@@ -5,7 +5,6 @@ import { h } from "./dom.js";
 export function log_append(text, isSystem = false) {
     console.info('log_append',text);
     const div = document.querySelector('#Log .tab-content');
-    // div.className = isSystem ? 'log-line system-msg' : 'log-line';
     const timestamp = new Date().toLocaleTimeString();
     const row = h('pre.log-row',text);
     div.appendChild(row);
